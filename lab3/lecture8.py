048d85d639ef500b9621eb674e3477dfc3f7c1dc
Grechnev Sergey <sgrecha10@gmail.com>
2020-05-25 15:00:17 +0300

    Еще добавили пушку

--- lab3/lecture8.py
+++ lab3/lecture8.py
@@ -11,11 +11,18 @@ class Ball:
         self.r = randint(20, 50)
         self.x = randint(self.r, WIDTH - self.r)
         self.y = randint(self.r, HEIGHT - self.r)
-        self.dx = randint(2, 5)
-        self.dy = randint(2, 5)
+        self.dx = randint(-5, 5)
+        self.dy = randint(-5, 5)
+        """
         self.color = "#%02x%02x%02x" % (randint(0, 255),
                                         randint(0, 255),
                                         randint(0, 255))
+        """
+        #self.w = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+        self.w = tuple((randint(0, 255) for i in range(3)))
+
+        self.color = "#%02x%02x%02x" % (self.w)
         self.ball_id = c.create_oval(self.x - self.r,
                                      self.y - self.r,
                                      self.x + self.r,
@@ -47,12 +54,15 @@ def main():
     c = tk.Canvas(root, width=WIDTH, height=HEIGHT)
     c.pack()
 
-    #bs = [Ball() for i in range(5)]
+    bs = [Ball() for i in range(40)]
 
+    """
     bs = []
-    for i in range(20):
+    for i in range(5):
         bs.append(Ball())
+    """
 
+    print((bs))
 
     tick()
     root.mainloop()
